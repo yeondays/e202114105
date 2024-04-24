65fff675abbfc587268f57addad4a5e568de8684
WNY <na010305@gmail.com>
2024-04-24 12:55:29 +0900

    fifth

--- src/e202114105/Hello.java
+++ src/e202114105/Hello.java
@@ -1,7 +1,8 @@
 package e202114105;
 
 public class Hello {
-    public static void run() {
-        System.out.println("hello world");
-    }
+	public static void run() {
+		System.out.println("hello world");
+		System.out.println("원나연");
+	}
 }
